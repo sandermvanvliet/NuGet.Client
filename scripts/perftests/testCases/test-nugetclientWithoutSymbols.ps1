3c74278f999dfa02d2f7622dc099d55e4844eb77
Cristina Manu <cmanu@microsoft.com>
2018-12-03 14:10:13 -0800

    Add new test.

---
+++ scripts/perftests/testCases/test-nugetclientWithoutSymbols.ps1
@@ -0,0 +1,31 @@
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]$nugetClient,
+    [Parameter(Mandatory=$true)]
+    [string]$sourceRootDirectory,
+    [Parameter(Mandatory=$true)]
+    [string]$resultsDirectoryPath,
+    [Parameter(Mandatory=$true)]
+    [string]$logsPath,
+    [Parameter(Mandatory=$false)]
+    [string]$testNamePrefix,
+    [Parameter(Mandatory=$false)]
+    [int]$iterationCount = 1000
+)
+
+
+    . "$PSScriptRoot\..\PerformanceTestUtilities.ps1"
+    
+    $repoUrl = "https://github.com/cristinamanum/PerfTestClientWS.git"
+    $testCaseName = GenerateNameFromGitUrl $repoUrl
+    
+    $resultsFilePath = [System.IO.Path]::Combine($resultsDirectoryPath, "Restore$testCaseName.csv")
+    
+    $solutionFilePath = SetupGitRepository -repository $repoUrl -commitHash $commitHash -sourceDirectoryPath  $([System.IO.Path]::Combine($sourceRootDirectory, $testCaseName))
+
+    $name = "Symbols_NuGetClient"
+    # it will be downloaded multiple packages  
+    $packageDownloadPath = ""
+    # run only clean restore
+    SetupNuGetFolders $nugetClient
+    . "$PSScriptRoot\..\RunPerformanceTests.ps1" $nugetClient $solutionFilePath $resultsFilePath $packageDownloadPath $logsPath $name  -skipWarmup -skipColdRestores -skipForceRestores -skipNoOpRestores -iterationCount $iterationCount
