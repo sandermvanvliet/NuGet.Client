547dfd4746ba434038db9f5b2f9bc872f6ddbd3c
Nikolche Kolev <nikolev@microsoft.com>
2019-01-08 18:48:54 -0800

    more removal of pack related info from the PackageSpec

--- test/NuGet.Core.Tests/NuGet.ProjectModel.Test/PackageSpecWriterTests.cs
+++ test/NuGet.Core.Tests/NuGet.ProjectModel.Test/PackageSpecWriterTests.cs
@@ -67,24 +67,7 @@ namespace NuGet.ProjectModel.Test
         {
             // Arrange
             var json = @"{
-  ""title"": ""My Title"",
   ""version"": ""1.2.3"",
-  ""description"": ""test"",
-  ""authors"": [
-    ""author1"",
-    ""author2""
-  ],
-  ""copyright"": ""2016"",
-  ""language"": ""en-US"",
-  ""tags"": [
-    ""tag1"",
-    ""tag2""
-  ],
-  ""projectUrl"": ""http://my.url.com"",
-  ""iconUrl"": ""http://my.url.com"",
-  ""summary"": ""Sum"",
-  ""releaseNotes"": ""release noted"",
-  ""licenseUrl"": ""http://my.url.com"",
   ""scripts"": {
     ""script1"": [
       ""script.js""
@@ -453,23 +436,10 @@ namespace NuGet.ProjectModel.Test
 
             var packageSpec = new PackageSpec()
             {
-                Authors = unsortedArray,
-                BuildOptions = new BuildOptions() { OutputName = "outputName" },
                 ContentFiles = new List<string>(unsortedArray),
-                Copyright = "copyright",
                 Dependencies = new List<LibraryDependency>() { libraryDependency, libraryDependencyWithNoWarnGlobal },
-                Description = "description",
                 FilePath = "filePath",
-                HasVersionSnapshot = true,
-                IconUrl = "iconUrl",
-                IsDefaultVersion = false,
-                Language = "language",
-                LicenseUrl = "licenseUrl",
                 Name = "name",
-                Owners = unsortedArray,
-                ProjectUrl = "projectUrl",
-                ReleaseNotes = "releaseNotes",
-                RequireLicenseAcceptance = true,
                 RestoreMetadata = new ProjectRestoreMetadata()
                 {
                     CrossTargeting = true,
@@ -493,9 +463,6 @@ namespace NuGet.ProjectModel.Test
                             new ProjectRestoreMetadataFrameworkInfo(nugetFramework)
                         }
                 },
-                Summary = "summary",
-                Tags = unsortedArray,
-                Title = "title",
                 Version = new NuGetVersion("1.2.3")
             };
 
